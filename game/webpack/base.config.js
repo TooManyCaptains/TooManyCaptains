525cdf6a1bf0713b3894363442b6de0cff53ea0c
Avi Romanoff <avi@romanoff.me>
2018-02-10 01:48:47 -0500

    game: fix webpack config

--- game/webpack/base.config.js
+++ game/webpack/base.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname + '../dist'),
+    path: path.join(__dirname + '/../dist'),
   },
 
   devtool: 'cheap-module-source-map',
