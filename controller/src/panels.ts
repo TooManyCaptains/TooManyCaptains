e12963554ed3ec9dfaaf907704e98ba64ad6db10
Avi Romanoff <avi@romanoff.me>
2018-03-01 01:22:06 -0500

    controller: fix lights for new scheme

--- controller/src/panels.ts
+++ controller/src/panels.ts
@@ -6,7 +6,7 @@ import { GameState } from '../../common/types';
 class WeaponsPanel extends Panel {
   public readonly name = 'weapons';
   public readonly pins = [40, 38, 36];
-  public readonly lightIndicies = _.range(7);
+  public readonly lightIndicies = _.range(6);
   public readonly buttonLightPins = [];
 
   public update(colorPositions: ColorPosition[], gameState: GameState): void {
@@ -14,33 +14,29 @@ class WeaponsPanel extends Panel {
     _.forEach(this.buttonLightPins, pin => {
       rpio.write(pin, isButtonLit ? rpio.HIGH : rpio.LOW);
     });
+
     // Set LED lights for later batch-update
     this.lights = [];
+    const pixelsPerPosition = Math.floor(
+      this.lightIndicies.length / this.pins.length,
+    );
     colorPositions
       .filter(({ position }) => position !== null)
-      .map(({ color, position }) => {
-        this.lights.push({
-          index: this.lightIndicies[position!],
-          color: LightColor[color],
-        });
-        this.lights.push({
-          index: this.lightIndicies[position! + 1],
-          color: LightColor[color],
+      .forEach(({ color, position }) => {
+        _.range(pixelsPerPosition).forEach(i => {
+          this.lights.push({
+            index: this.lightIndicies[i + position! * pixelsPerPosition],
+            color: LightColor[color],
+          });
         });
       });
-    // this.lights = colorPositions
-    //   .filter(({ position }) => position !== null)
-    //   .map(({ color, position }) => ({
-    //     index: this.lightIndicies[position!],
-    //     color: LightColor[color],
-    //   }));
   }
 }
 
 class ThrustersPanel extends Panel {
   public readonly name = 'thrusters';
-  public readonly pins = [16, 18];
-  public readonly lightIndicies = _.range(11, 15);
+  public readonly pins = [18, 16];
+  public readonly lightIndicies = _.range(9, 13);
   public readonly buttonLightPins = [];
 
   public update(colorPositions: ColorPosition[], gameState: GameState) {
@@ -49,46 +45,81 @@ class ThrustersPanel extends Panel {
       rpio.write(pin, isButtonLit ? rpio.HIGH : rpio.LOW);
     });
 
-    this.lights = _.times(colorPositions.length, i => ({
-      index: this.lightIndicies[i],
-      color: LightColor.purple,
-    }));
-  }
-}
-
-class ShieldsPanel extends Panel {
-  public readonly name = 'shields';
-  public readonly pins = [22, 26, 24];
-  public readonly lightIndicies = _.range(19, 26);
-
-  public update(colorPositions: ColorPosition[]): void {
-    this.lights = colorPositions
+    // Set LED lights for later batch-update
+    this.lights = [];
+    const pixelsPerPosition = Math.floor(
+      this.lightIndicies.length / this.pins.length,
+    );
+    colorPositions
       .filter(({ position }) => position !== null)
-      .map(({ color, position }) => ({
-        index: this.lightIndicies[position!],
-        color: LightColor[color],
-      }));
+      .forEach(({ color, position }) => {
+        _.range(pixelsPerPosition).forEach(i => {
+          this.lights.push({
+            index: this.lightIndicies[i + position! * pixelsPerPosition],
+            color: LightColor.purple,
+          });
+        });
+      });
   }
 }
 
 class RepairsPanel extends Panel {
   public readonly name = 'repairs';
-  public readonly pins = [10, 12, 8];
-  public readonly lightIndicies = _.range(30, 37);
+  public readonly pins = [26, 24, 22];
+  public readonly lightIndicies = _.range(16, 22);
 
   public update(colorPositions: ColorPosition[]): void {
-    this.lights = _.times(colorPositions.length, i => ({
-      index: this.lightIndicies[i],
-      color: LightColor.green,
-    }));
+    // this.lights = _.times(colorPositions.length, i => ({
+    //   index: this.lightIndicies[i],
+    //   color: LightColor.green,
+    // }));
+    // Set LED lights for later batch-update
+    this.lights = [];
+    const pixelsPerPosition = Math.floor(
+      this.lightIndicies.length / this.pins.length,
+    );
+    colorPositions
+      .filter(({ position }) => position !== null)
+      .forEach(({ color, position }) => {
+        _.range(pixelsPerPosition).forEach(i => {
+          this.lights.push({
+            index: this.lightIndicies[i + position! * pixelsPerPosition],
+            color: LightColor.green,
+          });
+        });
+      });
+  }
+}
+
+class ShieldsPanel extends Panel {
+  public readonly name = 'shields';
+  public readonly pins = [8, 12, 10];
+  public readonly lightIndicies = _.range(25, 32);
+
+  public update(colorPositions: ColorPosition[]): void {
+    // Set LED lights for later batch-update
+    this.lights = [];
+    const pixelsPerPosition = Math.floor(
+      this.lightIndicies.length / this.pins.length,
+    );
+    colorPositions
+      .filter(({ position }) => position !== null)
+      .forEach(({ color, position }) => {
+        _.range(pixelsPerPosition).forEach(i => {
+          this.lights.push({
+            index: this.lightIndicies[i + position! * pixelsPerPosition],
+            color: LightColor[color],
+          });
+        });
+      });
   }
 }
 
 const panels: Panel[] = [
   new WeaponsPanel(),
   new ThrustersPanel(),
-  new ShieldsPanel(),
   new RepairsPanel(),
+  new ShieldsPanel(),
 ];
 
 export { panels };
