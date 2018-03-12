2f91aad3ad4063a1fa9e14d1e5594aaa27f7d18e
Avi Romanoff <avi@romanoff.me>
2018-03-12 10:46:20 -0500

    game: refactor: dispatch packets directly to network-concerned states

--- game/src/states/Before.ts
+++ game/src/states/Before.ts
@@ -1,8 +1,10 @@
+import NetworkedState from './NetworkedState';
 import { StartScreen } from '../interface/Screens';
 import Doors from '../interface/Doors';
 import { Game } from '../index';
+import { Packet } from '../../../common/types';
 
-export default class Boot extends Phaser.State {
+export default class Before extends NetworkedState {
   public game: Game;
 
   public preload() {
@@ -56,12 +58,33 @@ export default class Boot extends Phaser.State {
     );
   }
 
+  public onPacket(packet: Packet) {
+    if (packet.kind === 'fire') {
+      if (packet.state === 'released') {
+        if (this.game.captains.length >= 2) {
+          this.state.start('Main');
+        }
+      }
+    } else if (packet.kind === 'scan') {
+      const captain = this.game.captains.find(
+        cardID => cardID === packet.cardID,
+      );
+      if (!captain && packet.cardID !== 0) {
+        this.game.captains.push(packet.cardID);
+      } else {
+        // TODO: add engineer
+      }
+    }
+  }
+
   public create() {
     this.game.add.existing(new Doors(this.game));
     this.game.add.existing(new StartScreen(this.game));
 
     // Update gamestate
     this.game.gameState = 'wait_for_players';
-    this.game.add.audio('music_background').play(undefined, undefined, undefined, true);
+    this.game.add
+      .audio('music_background')
+      .play(undefined, undefined, undefined, true);
   }
 }
