3a4e361e841a456d16c020a093905f31fcebf6bc
Avi Romanoff <avi@romanoff.me>
2018-03-14 15:50:12 -0500

    game: map: add sprites to group, not world

--- game/src/interface/Map.ts
+++ game/src/interface/Map.ts
@@ -2,20 +2,20 @@ import { Game } from '../index';
 
 export default class Map extends Phaser.Group {
   public game: Game;
-  public miniMap: Phaser.Sprite;
-  public iconPlayer: Phaser.Sprite;
-  public iconBoss: Phaser.Sprite;
+
+  private miniMap: Phaser.Sprite;
+  private iconPlayer: Phaser.Sprite;
+  // private iconBoss: Phaser.Sprite;
 
   constructor(game: Game) {
     super(game);
 
     // Sprites
-    this.miniMap = this.create(0, 0, 'map');
-    this.iconPlayer = this.create(100, 0, 'map-icon-player');
+    this.miniMap = new Phaser.Sprite(this.game, 0, 0, 'map');
+    this.iconPlayer = new Phaser.Sprite(this.game, 100, 0, 'map-icon-player');
     this.iconPlayer.anchor.setTo(0.5, 0);
 
+    this.add(this.miniMap);
+    this.add(this.iconPlayer);
   }
-  
-  
-
 }
