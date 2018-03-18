3ec293935453b46d43fe66796188d08b05381c47
Avi Romanoff <avi@romanoff.me>
2018-03-18 12:30:39 -0700

    game: flip score label

--- game/src/interface/Map.ts
+++ game/src/interface/Map.ts
@@ -1,46 +1,69 @@
 import { Game } from '../index';
 import { baseStyle, ColorPalette } from './Styles';
 
-const MIN_X = 50;
+const MIN_X = 1600;
 const MAX_X = 1870;
 
-class ScoreBubble extends Phaser.Group {
+class Bubble extends Phaser.Group {
   public game: Game;
   private text: Phaser.Text;
   private background: Phaser.Graphics;
+  private iconPlayer: Phaser.Sprite;
+
+  private readonly topLabelOffset = 25;
+  private leftLabelOffset = 40;
 
   constructor(game: Game) {
     super(game);
-    this.text = this.game.add.text(20, 4, '', {
-      ...baseStyle,
-      fontSize: 32,
-    });
+    this.text = this.game.add.text(
+      this.leftLabelOffset + 20,
+      this.topLabelOffset + 4,
+      '',
+      {
+        ...baseStyle,
+        fontSize: 32,
+      },
+    );
     this.text.align = 'left';
 
     this.background = game.add.graphics();
 
+    this.iconPlayer = new Phaser.Sprite(this.game, 0, 0, 'map-icon-player');
+    this.iconPlayer.anchor.setTo(0.5, 0);
+    this.iconPlayer.scale.setTo(0.75, 0.75);
+
     this.add(this.background);
     this.add(this.text);
+    this.add(this.iconPlayer);
 
     this.game.session.signals.score.add(this.onScoreChanged, this);
 
     this.onScoreChanged();
   }
 
+  public update() {
+    if (this.game.width - this.x < 275) {
+      this.text.left = this.leftLabelOffset + 20;
+      this.leftLabelOffset = -240;
+      this.onScoreChanged();
+    }
+    super.update();
+  }
+
   private onScoreChanged() {
     this.background.clear();
     this.background.beginFill(ColorPalette.Black, 0.7);
     this.background.drawRoundedRect(
-      0,
-      0,
+      this.leftLabelOffset,
+      this.topLabelOffset,
       165 + 15 * String(this.game.session.score).length,
       50,
       25,
     );
     this.background.lineStyle(2, ColorPalette.White, 1);
     this.background.drawRoundedRect(
-      0,
-      0,
+      this.leftLabelOffset,
+      this.topLabelOffset,
       165 + 15 * String(this.game.session.score).length,
       50,
       25,
@@ -56,8 +79,7 @@ class ScoreBubble extends Phaser.Group {
 export default class Map extends Phaser.Group {
   public game: Game;
   private miniMap: Phaser.Sprite;
-  private scoreBubble: ScoreBubble;
-  private iconPlayer: Phaser.Sprite;
+  private bubble: Bubble;
   // private iconBoss: Phaser.Sprite;
 
   constructor(game: Game) {
@@ -65,19 +87,15 @@ export default class Map extends Phaser.Group {
 
     // Sprites
     this.miniMap = new Phaser.Sprite(this.game, 0, 0, 'map');
-    this.iconPlayer = new Phaser.Sprite(this.game, MIN_X, 0, 'map-icon-player');
-    this.iconPlayer.anchor.setTo(0.5, 0);
-    this.iconPlayer.scale.setTo(0.75, 0.75);
-    this.scoreBubble = new ScoreBubble(this.game);
-    this.scoreBubble.y = 25;
-    this.scoreBubble.x = 100;
+    this.bubble = new Bubble(this.game);
+    this.bubble.y = 10;
+    this.bubble.x = MIN_X;
 
     this.add(this.miniMap);
-    this.add(this.iconPlayer);
-    this.add(this.scoreBubble);
+    this.add(this.bubble);
 
     this.game.add
-      .tween(this.iconPlayer)
+      .tween(this.bubble)
       .to(
         { x: MAX_X },
         this.game.session.totalTimeToBoss,
@@ -85,17 +103,17 @@ export default class Map extends Phaser.Group {
         true,
       );
 
-    // Unfortunately, this tween will not be perfectly smooth, since we are
-    // tweening text and it will align to pixels, rather than draw on subpixels
-    // it seems. Even rolling our own linear interpolation by overriding
-    // update() doesn't fix it :/
-    this.game.add
-      .tween(this.scoreBubble)
-      .to(
-        { x: MAX_X },
-        this.game.session.totalTimeToBoss,
-        Phaser.Easing.Linear.None,
-        true,
-      );
+    // // Unfortunately, this tween will not be perfectly smooth, since we are
+    // // tweening text and it will align to pixels, rather than draw on subpixels
+    // // it seems. Even rolling our own linear interpolation by overriding
+    // // update() doesn't fix it :/
+    // this.game.add
+    //   .tween(this.scoreBubble)
+    //   .to(
+    //     { x: 1625 },
+    //     this.game.session.totalTimeToBoss,
+    //     Phaser.Easing.Linear.None,
+    //     true,
+    //   );
   }
 }
